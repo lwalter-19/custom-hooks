88035411a80fc1ce53659ee013b46fccf22eefe0
Hazael Lopez <elhazael134@gmail.com>
2022-12-16 12:39:20 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+const initialState = [];
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+
+    const handleAddTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length, 
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleAddTodo
+    }
+}
